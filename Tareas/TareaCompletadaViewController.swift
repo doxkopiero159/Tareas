b03bb652d59f88a2e1020449291f8d0c90b43ad6
proyecto integrador <piero.granda@tecsup.edu.pe>
2019-04-12 21:52:01 -0500

    primer commit

---
+++ Tareas/TareaCompletadaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TareaCompletadaViewController.swift
+//  Tareas
+//
+//  Created by piero  acxel on 12/04/19.
+//  Copyright © 2019 piero  acxel. All rights reserved.
+//
+
+import UIKit
+
+class TareaCompletadaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
